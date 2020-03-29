c8c5bf23f5125981be276ae8134f43faddf5b71b
BenZina <slaheddine.ben.zina@gmail.com>
2020-03-30 00:16:04 +0200

    starting clean archi

---
+++ app/src/main/java/me/slaheddine/leboncoin/data/models/AlbumData.kt
@@ -0,0 +1,9 @@
+package me.slaheddine.leboncoin.data.models
+
+data class AlbumData(
+    val albumId: Int,
+    val id: Int,
+    val thumbnailUrl: String,
+    val title: String,
+    val url: String
+)
---
+++ app/src/main/java/me/slaheddine/leboncoin/domain/models/Album.kt
@@ -0,0 +1,9 @@
+package me.slaheddine.leboncoin.domain.models
+
+data class Album(
+    val albumId: Int,
+    val id: Int,
+    val thumbnailUrl: String,
+    val title: String,
+    val url: String
+)
